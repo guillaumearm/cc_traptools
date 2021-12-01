c5fd7e46e007544e852c82e084a1f0521d98573e
Guillaume ARM <trapcodien@gmail.com>
2021-12-01 15:45:37 +0100

    feat: add install script

--- install.lua
+++ install.lua
@@ -1,4 +1,14 @@
+local REPO_PREFIX = 'https://raw.githubusercontent.com/guillaumearm/cc_traptools/master'
+
 fs.makeDir('/apis');
 fs.makeDir('/daemons');
 fs.makeDir('/lib');
 fs.makeDir('/bin');
+
+local LIST_FILES = {'/startup.lua', '/apis/bapil', 'apis/logger', '/apis/daemon', '/bin/daemon.lua'}
+
+for k, filePath in pairs(LIST_FILES) do
+    shell.execute('wget', REPO_PREFIX .. filePath, filePath)
+end
+
+shell.execute('/startup.lua')
