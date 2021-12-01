b17f8e67dcbac83529053ecfa074129b8c22f2e8
Guillaume ARM <trapcodien@gmail.com>
2021-12-01 20:53:02 +0100

    fix(cat): use print instead of write

--- bin/cat.lua
+++ bin/cat.lua
@@ -1,23 +1,23 @@
-local given_filepath = ...
+local given_filepath = ...;
 
 if not given_filepath then
-  error('usage: cat <filepath>')
+  error('usage: cat <filepath>');
 end
 
 -- TODO: autocompletion
 
-local filepath = shell.dir() .. '/' .. given_filepath
+local filepath = shell.dir() .. '/' .. given_filepath;
 
 if not fs.exists(filepath) then
-  error("'" .. filepath .. "' does not exists")
+  error("'" .. filepath .. "' does not exists");
 end
 
 if fs.isDir(filepath) then
-  error("'" .. filepath .. "' is a directory")
+  error("'" .. filepath .. "' is a directory");
 end
 
-local handler = fs.open(filepath, "r")
-local data = handler.readAll()
+local handler = fs.open(filepath, "r");
+local data = handler.readAll();
 handler.close();
 
-write(data)
+print(data);
