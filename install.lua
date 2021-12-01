9df58173069d63b7d53ba352a3fc0a20c8136d08
Guillaume ARM <trapcodien@gmail.com>
2021-12-01 15:55:11 +0100

    feat(install): try with relative path

--- install.lua
+++ install.lua
@@ -1,14 +1,18 @@
-local REPO_PREFIX = 'https://raw.githubusercontent.com/guillaumearm/cc_traptools/master'
+local LIST_FILES = {'startup.lua', 'apis/bapil', 'apis/logger', 'apis/daemon', 'bin/daemon.lua'}
+
+local REPO_PREFIX = 'https://raw.githubusercontent.com/guillaumearm/cc_traptools/master/'
 
 fs.makeDir('/apis');
 fs.makeDir('/daemons');
 fs.makeDir('/lib');
 fs.makeDir('/bin');
 
-local LIST_FILES = {'/startup.lua', '/apis/bapil', '/apis/logger', '/apis/daemon', '/bin/daemon.lua'}
-
 for k, filePath in pairs(LIST_FILES) do
+    fs.delete(filePath)
     shell.execute('wget', REPO_PREFIX .. filePath, filePath)
 end
 
+print()
+
+print('=> Execute startup.lua')
 shell.execute('/startup.lua')
