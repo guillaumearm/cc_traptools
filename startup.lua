f71820b715267cc33815f5873307f499c1d7d191
Guillaume ARM <trapcodien@gmail.com>
2021-12-01 17:45:47 +0100

    feat: libevents

--- startup.lua
+++ startup.lua
@@ -1,8 +1,21 @@
+local APIS_TO_LOAD = {'apis/events'}
+
+-- 0. add /bin in path
 shell.setPath(shell.path(0) .. ":/bin")
 
+-- 1. additional aliases
+shell.setAlias("c", "clear")
+
+-- 2. hijack os loadAPI
 os.loadAPI("apis/bapil")
 bapil.hijackOSAPI()
 
+-- 3. load common apis
+for _, apipath in ipairs(APIS_TO_LOAD) do
+  assert(os.loadAPI(apipath))
+end
+
+-- 4. load and install daemon api
 assert(os.loadAPI("apis/daemon"))
 daemon.install()
 
