6bedc05e0f33dc49d0c81482026f1c70347c5312
Guillaume ARM <trapcodien@gmail.com>
2021-12-01 15:49:09 +0100

    fix: install script

--- install.lua
+++ install.lua
@@ -5,7 +5,7 @@ fs.makeDir('/daemons');
 fs.makeDir('/lib');
 fs.makeDir('/bin');
 
-local LIST_FILES = {'/startup.lua', '/apis/bapil', 'apis/logger', '/apis/daemon', '/bin/daemon.lua'}
+local LIST_FILES = {'/startup.lua', '/apis/bapil', '/apis/logger', '/apis/daemon', '/bin/daemon.lua'}
 
 for k, filePath in pairs(LIST_FILES) do
     shell.execute('wget', REPO_PREFIX .. filePath, filePath)
