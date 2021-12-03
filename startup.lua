c9137fd6c9e2cc6ca8dcdd7e2255f52516c67aba
Guillaume ARM <trapcodien@gmail.com>
2021-12-03 21:02:19 +0100

    feat(apis): implement msequencer + improve clock and eventloop

--- startup.lua
+++ startup.lua
@@ -1,5 +1,5 @@
 local APIS_TO_LOAD = {'apis/eventloop', 'apis/net', 'apis/colorutils', 'apis/rsw', 'apis/rsr', 'apis/rsclient',
-                      'apis/chatbox', 'apis/mclock'}
+                      'apis/chatbox', 'apis/mclock', 'apis/msequencer'}
 
 -- 0. add /bin in path
 shell.setPath(shell.path(0) .. ":/bin")
