dcf3de85521e2bc5a81c37f844c57e6363b4f4bc
Guillaume ARM <trapcodien@gmail.com>
2021-12-04 19:15:33 +0100

    fix: install script

--- install.lua
+++ install.lua
@@ -1,6 +1,6 @@
 local LIST_FILES = {'startup.lua', 'apis/bapil', 'apis/stacktrace', 'apis/eventloop', 'apis/colorutils', 'apis/rsw',
                     'apis/rsr', 'apis/rsclient', 'apis/net', 'apis/chatbox', 'apis/mclock', 'apis/msequencer',
-                    'apis/logger', 'apis/daemon', 'bin/cat.lua', 'bin/daemon.lua', 'bin/seq'};
+                    'apis/logger', 'apis/daemon', 'bin/cat.lua', 'bin/daemon.lua', 'bin/seq.lua'};
 
 local DAEMON_LIST = {'redserver', 'sdoors', 'emoji', 'rspeaker'}
 
