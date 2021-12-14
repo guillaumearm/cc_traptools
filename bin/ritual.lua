4b862450bdc2884d5877bf16b499b46bf44969fa
Guillaume ARM <trapcodien@gmail.com>
2021-12-15 00:22:24 +0100

    feat(bin): save ritual program

---
+++ bin/ritual.lua
@@ -0,0 +1,51 @@
+local chat = peripheral.find('chatBox');
+local t = peripheral.wrap('right');
+
+function sendFormattedMessage(payload, prefix)
+  prefix = prefix or 'Trap Weather Bot';
+  chat.sendFormattedMessage(textutils.serializeJSON(payload), prefix);
+end
+
+local function doRitual(slot)
+  sendFormattedMessage({
+    text = "OK, processing...",
+    color = 'yellow'
+  })
+  turtle.select(slot);
+  t.useOnBlock();
+  turtle.select(16);
+  os.sleep(2)
+  t.useOnBlock();
+  sendFormattedMessage({
+    text = "ritual started!",
+    color = 'yellow'
+  })
+
+  os.sleep(18);
+  sendFormattedMessage({
+    text = "ritual finished!",
+    color = 'green'
+  })
+end
+
+print('=> Rituals server started.')
+
+while true do
+  local event, username, message = os.pullEvent('chat');
+
+  if message == '!day' then
+    print('=> day command received');
+    doRitual(1);
+  elseif message == '!stoprain' then
+    print('=> stoprain command received');
+    doRitual(2);
+  elseif message == '!night' then
+    print('=> night command received');
+    doRitual(3);
+  elseif message == '!help' then
+    sendFormattedMessage({
+      text = "\n`!day` start a sunrise ritual\n`!stoprain` start a cloudshaping ritual\n`!night` start a moonfall ritual",
+      color = 'green'
+    })
+  end
+end
